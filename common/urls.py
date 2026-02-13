51069b623cac542aaf369603b0d95297db1fd0a5
NikolinaPetrova <nina.petrowa94@gmail.com>
2026-02-13 17:45:25 +0200

    Refactor search: handle queryset directly in HomePageView

--- common/urls.py
+++ common/urls.py
@@ -3,5 +3,4 @@ from common import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('search/', views.SearchArtworksView.as_view(), name='search-artworks'),
 ]
--- common/views.py
+++ common/views.py
@@ -9,21 +9,6 @@ class HomePageView(ListView):
     template_name = 'common/home-page.html'
 
     def get_queryset(self):
-        return Artwork.objects.select_related('user').prefetch_related('tags')
-
-class Custom404View(TemplateView):
-    template_name = '404.html'
-
-    def render_to_response(self, context, **response_kwargs):
-        response_kwargs['status'] = 404
-        return super().render_to_response(context, **response_kwargs)
-
-class SearchArtworksView(ListView):
-    model = Artwork
-    template_name = 'artwork/gallery.html'
-    context_object_name = 'artwork_list'
-
-    def get_queryset(self):
         query = self.request.GET.get('q', '').strip()
         queryset = Artwork.objects.select_related('user').prefetch_related('tags')
 
@@ -39,3 +24,10 @@ class SearchArtworksView(ListView):
             q_object |= Q(tags__name__icontains=word)
 
         return queryset.filter(q_object).distinct()
+
+class Custom404View(TemplateView):
+    template_name = '404.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        response_kwargs['status'] = 404
+        return super().render_to_response(context, **response_kwargs)
