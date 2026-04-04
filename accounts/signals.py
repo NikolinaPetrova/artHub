e2ffe4fe15f5ec1665ba20bb5d04f58a8898d023
NikolinaPetrova <nina.petrowa94@gmail.com>
2026-04-04 20:02:29 +0300

    fix: add post_migrate signal for group permissions

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from accounts.models import Profile
 from albums.models import Album
@@ -19,3 +20,44 @@ def create_default_album(sender, instance, created, **kwargs):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_migrate)
+def assign_permissions_to_content_groups(sender, **kwargs):
+    if sender.name != 'accounts':
+        return
+
+    moderators_group, _ = Group.objects.get_or_create(name='Content Moderator')
+    editors_group, _ = Group.objects.get_or_create(name='Content Editor')
+
+    editors_permissions = [
+        ('artworks', 'change_artwork'),
+        ('albums', 'change_album'),
+        ('interactions', 'change_comment'),
+        ('groups', 'change_post'),
+        ('groups', 'change_group')
+    ]
+
+    moderators_permissions = [
+        ('accounts', 'delete_arthubuser'),
+        ('accounts', 'delete_profile'),
+        ('artworks', 'delete_artwork'),
+        ('albums', 'delete_album'),
+        ('interactions', 'delete_comment'),
+        ('groups', 'delete_post'),
+        ('groups', 'delete_group'),
+    ]
+
+    for app_label, codename in editors_permissions:
+        try:
+            perm = Permission.objects.get(codename=codename, content_type__app_label=app_label)
+            editors_group.permissions.add(perm)
+        except Permission.DoesNotExist:
+            pass
+
+    for app_label, codename in moderators_permissions:
+        try:
+            perm = Permission.objects.get(codename=codename, content_type__app_label=app_label)
+            moderators_group.permissions.add(perm)
+        except Permission.DoesNotExist:
+            pass
